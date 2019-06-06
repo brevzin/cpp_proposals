31161c67aec434b89db9969990b187481e85a76d
Barry Revzin <barry.revzin@gmail.com>
2019-06-05 22:42:24 -0500

    Adding a makefile script instead of the bash script.

---
+++ md/mpark-wg21.mk
@@ -0,0 +1,20 @@
+THIS_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+MPARK_DATA := $(THIS_DIR)wg21/data
+
+PANDOC = pandoc $< $(MPARK_DATA)/references.md \
+   --number-sections \
+   --self-contained \
+   --table-of-contents \
+   --bibliography $(MPARK_DATA)/index.yaml \
+   --csl $(MPARK_DATA)/cpp.csl \
+   --css $(MPARK_DATA)/template/14882.css \
+   --css $(THIS_DIR)/pandoc.css \
+   --filter pandoc-citeproc \
+   --filter $(MPARK_DATA)/filter/wg21.py \
+   --filter $(THIS_DIR)/pandoc.py \
+   --highlight-style $(MPARK_DATA)/syntax/wg21.theme \
+   --metadata datadir:$(MPARK_DATA) \
+   --metadata-file $(MPARK_DATA)/metadata.yaml \
+   --syntax-definition $(MPARK_DATA)/syntax/isocpp.xml \
+   --template $(MPARK_DATA)/template/wg21 \
+   --output $@
