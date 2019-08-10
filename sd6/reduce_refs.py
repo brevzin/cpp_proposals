59a2d01fe50ea289aa194bd481d840135f2842a2
Barry Revzin <barry.revzin@gmail.com>
2019-08-10 14:55:48 -0500

    Ading P1353: missing macros.

--- sd6/reduce_refs.py
+++ sd6/reduce_refs.py
@@ -1,21 +1,11 @@
 import json
 import yaml
 
-macros = yaml.load(open('macros.yaml'), Loader=yaml.Loader)
-
 def refs_to_dict(filename):
     data = yaml.load(open(filename), Loader=yaml.Loader)
-    return {elem['id']: elem for elem in data['references']}
+    return {elem['id']: elem['title'] for elem in data['references']}
 
 refs = refs_to_dict('../md/wg21/data/index.yaml')
 refs.update(refs_to_dict('../md/wg21_fmt.yaml'))
 
-reduced_refs = {}
-for top in ('language', 'library', 'attributes'):
-    for elem in macros[top]:
-        for value in elem['values']:
-            if value.get('papers'):
-                for paper in value['papers'].split():
-                    reduced_refs[paper] = refs[paper]['title']
-
-print(json.dumps(reduced_refs))
+print(json.dumps(refs))
