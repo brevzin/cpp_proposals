6e1f8e19a8c61c28a852ac5c407a399765dca83b
Barry Revzin <barry.revzin@gmail.com>
2019-08-13 18:59:43 -0500

    Adding headers for the library macros.

--- sd6/sd6.py
+++ sd6/sd6.py
@@ -11,30 +11,8 @@ loader = jinja2.FileSystemLoader(searchpath='./')
 env = jinja2.Environment(loader=loader)
 template = env.get_template('sd6.tmpl')
 
-def refactor(macros):
-    # turn something like
-    # {'name': '__cpp_lib_byte', 'values': [{'value': 201603, 'papers': 'P0298R3'
-    # into
-    # {'name': '__cpp_lib_byte', 'value': {201603: ['P0298R3']}} 
-    seen = set()
-    for macro in macros:
-        if macro['name'] in seen:
-            print(f'Duplicate macro "{macro["name"]}"', file=sys.stderr)
-        seen.add(macro['name'])
-        try:
-            macro['value'] = {
-                v['value']: {
-                    'papers': v['papers'].split() if v.get('papers') else '',
-                    'feature': v.get('feature', ''),
-                    'removed': v.get('removed', False)
-                } for v in macro.pop('values')}
-        except KeyError as e:
-            print(f'{e} thrown for {macro}', file=sys.stderr)
-            raise
-    return sorted(macros, key=lambda m: m['name'])
-
 print(template.render(
-    lang_macros=refactor(data['language']),
-    lib_macros=refactor(data['library']),
-    attr_macros=refactor(data['attributes']),
+    lang_macros=data['language'],
+    lib_macros=data['library'],
+    attr_macros=data['attributes'],
     refs=refs))
