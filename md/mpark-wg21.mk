2485e32869e53505717ec699cbb16417a44159ec
Michael Park <mcypark@gmail.com>
2026-06-26 15:03:58 -0700

    Simplifications based on changes upstream (#295)
    * Remove inline code highlighting default since it's the default behavior upstream.
    
    * Remove no longer needed `defaults.py`.
    
    Turns out interpolation variables for defaults files have been available
    since Pandoc 2.12. Use it to clean up a bit.
    
    * Remove csl.json from the command line. It's included in defaults/doc.yaml now.
    
    * Move wg21_fmt.yaml bibliography to defaults.yaml.
    
    * Update mpark/wg21.
    
    * Use the per-paper project layout by including paper.mk.

--- md/mpark-wg21.mk
+++ md/mpark-wg21.mk
@@ -1,22 +1,3 @@
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-OUTDIR := .
 DEFAULTS := $(THIS_DIR)defaults.yaml
-include $(THIS_DIR)wg21/Makefile
-
-$(THIS_DIR)defaults.yaml : $(THIS_DIR)defaults.py
-	$< > $@
-
-ifdef NO_BIBLIO
-full_index :=
-else
-full_index := --bibliography $(DATADIR)/index.yaml
-endif
-
-#%.html : $(DEPS)
-#	$(PANDOC) \
-#    --bibliography $(THIS_DIR)wg21_fmt.yaml \
-#	$(full_index)
-
-$(OUTDIR)/p%.html $(OUTDIR)/d%.html : $(DEPS)
-	$(PANDOC) --bibliography $(DATADIR)/csl.json --bibliography $(THIS_DIR)wg21_fmt.yaml -f markdown-tex_math_dollars
+include $(THIS_DIR)wg21/paper.mk
