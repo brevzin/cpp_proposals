690f5c95aa0fb014eca9ea9ddb4684da09665cbc
Michael Park <mcypark@gmail.com>
2026-06-26 00:31:48 -0700

    Use the per-paper project layout by including paper.mk.

--- md/mpark-wg21.mk
+++ md/mpark-wg21.mk
@@ -1,19 +1,3 @@
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-OUTDIR := .
 DEFAULTS := $(THIS_DIR)defaults.yaml
-include $(THIS_DIR)wg21/Makefile
-
-ifdef NO_BIBLIO
-full_index :=
-else
-full_index := --bibliography $(DATADIR)/index.yaml
-endif
-
-#%.html : $(DEPS)
-#	$(PANDOC) \
-#    --bibliography $(THIS_DIR)wg21_fmt.yaml \
-#	$(full_index)
-
-$(OUTDIR)/p%.html $(OUTDIR)/d%.html : $(DEPS)
-	$(PANDOC) -f markdown-tex_math_dollars
+include $(THIS_DIR)wg21/paper.mk
